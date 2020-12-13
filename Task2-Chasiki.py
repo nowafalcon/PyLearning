6d435da4cf7bac3623bcba630911e508b88fb45d
nowafalcon <nowafalcon@gmail.com>
2020-12-13 16:53:46 +0400

    T2 updated

--- Task2-Chasiki.py
+++ Task2-Chasiki.py
@@ -2,25 +2,12 @@
 # минуты и секунды и выведите в формате чч:мм:сс. Используйте
 # форматирование строк.
 
-initial = int(input("Введите число = количество секунд: "))
-# days  86400secs
-# hrs   3600secs
-# mins  60secs
-# secs  1secs
-days = initial // 86400
-hrs = initial % 86400 // 3600
-mins = initial % 86400 % 3600 // 60
-secs = initial % 86400 % 3600 % 60
-if hrs < 10:
-    hrs = "0" + str(hrs)
+initial=int(input("Print number of seconds: "))
+days=initial//86400
+hrs=initial%86400//3600
+mins=initial%3600//60
+secs=initial%60
+if days!=0:
+    print(f"Days:{days} and {hrs:02}:{mins:02}:{secs:02}")
 else:
-    hrs = str(hrs)
-if mins < 10:
-    mins = "0" + str(mins)
-else:
-    mins = str(mins)
-if secs < 10:
-    secs = "0" + str(secs)
-else:
-    secs = str(secs)
-print("%s : %s : %s" % (hrs, mins, secs))
+    print(f"{hrs:02}:{mins:02}:{secs:02}")
