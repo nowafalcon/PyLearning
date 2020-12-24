73edb1d5318cb5732220c54b4465c03eaca51823
nowafalcon <nowafalcon@gmail.com>
2020-12-24 04:49:12 +0400

    L4 T1 Done

--- L4 T1.py
+++ L4 T1.py
@@ -1,3 +1,16 @@
 # Реализовать скрипт, в котором должна быть предусмотрена функция расчета заработной платы сотрудника.
 # В расчете необходимо использовать формулу: (выработка в часах * ставка в час) + премия. Для выполнения
 # расчета для конкретных значений необходимо запускать скрипт с параметрами.
+
+from sys import argv
+
+print(argv)
+try:
+    name, time, salary, bonus = argv
+    time = int(time)
+    salary = int(salary)
+    bonus = int(bonus)
+    res = time * salary + bonus
+    print(f'заработная плата сотрудника  {res}')
+except ValueError:
+    print('Not a number')
