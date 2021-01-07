8f085a7787aaa446016e72e27f76b38ea96033ca
nowafalcon <nowafalcon@gmail.com>
2021-01-07 22:11:08 +0300

    L5 T1 Done

--- Lesson5 Task1.py
+++ Lesson5 Task1.py
@@ -1,1 +1,16 @@
-# Lesson5 initiated
+# Создать программно файл в текстовом формате, записать в него построчно данные, вводимые пользователем.
+# Об окончании ввода данных свидетельствует пустая строка,
+
+my_f = open('test.txt', 'w')
+line = input('Введите текст: ')
+while line:
+    my_f.writelines(line+'\n')
+    line = input('Введите текст: ')
+    if not line:
+        break
+
+my_f.close()
+my_f = open('test.txt', 'r')
+content = my_f.readlines()
+print(content)
+my_f.close()
