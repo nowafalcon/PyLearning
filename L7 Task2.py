430289cbe9cb3303b52bef7e6337cf7ff5a2531d
nowafalcon <nowafalcon@gmail.com>
2021-01-18 01:29:38 +0300

    L7 T1 Done T2 in progress T3 initiated

---
+++ L7 Task2.py
@@ -0,0 +1,52 @@
+# Реализовать проект расчета суммарного расхода ткани на производство одежды. Основная сущность (класс)
+# этого проекта — одежда, которая может иметь определенное название. К типам одежды в этом проекте относятся
+# пальто и костюм. У этих типов одежды существуют параметры: размер (для пальто) и рост (для костюма). Это
+# могут быть обычные числа: V и H, соответственно.
+# Для определения расхода ткани по каждому типу одежды использовать формулы: для пальто (V/6.5 + 0.5), для
+# костюма (2 * H + 0.3). Проверить работу этих методов на реальных данных.
+# Реализовать общий подсчет расхода ткани. Проверить на практике полученные на этом уроке знания: реализовать
+# абстрактные классы для основных классов проекта, проверить на практике работу декоратора @property.
+
+class Tkan:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def get_square_c(self):
+        return self.width / 6.5 + 0.5
+
+    def get_square_j(self):
+        return self.height * 2 + 0.3
+
+    @property
+    def get_sq_full(self):
+        return str(f'Площадь общая ткани\n'
+                   f'{(self.width / 6.5 + 0.5) + (self.height * 2 + 0.3)}')
+
+
+class Coat(Tkan):
+    def __init__(self, width, height):
+        super().__init__(width, height)
+        self.square_c = round(self.width / 6.5 + 0.5)
+
+    def __str__(self):
+        return f'Площадь на пальто: {round(self.square_c,2)}'
+
+
+class Jacket(Tkan):
+    def __init__(self, width, height):
+        super().__init__(width, height)
+        self.square_j = round(self.height * 2 + 0.3)
+
+    def __str__(self):
+        return f'Площадь на костюм: {self.square_j}'
+
+
+coat = Coat(8, 10)
+jacket = Jacket(4, 6)
+print(coat)
+print(jacket)
+print(coat.get_sq_full)
+print(jacket.get_sq_full)
+print(jacket.get_square_c())
+print(jacket.get_square_j())
